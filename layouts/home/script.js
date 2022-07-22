d0de72e90faf76db85b5c7a4b4677909199526ed
dimden <firefox78888@gmail.com>
2022-07-22 14:52:24 +0300

    oops

--- layouts/home/script.js
+++ layouts/home/script.js
@@ -127,11 +127,6 @@ async function updateTimeline() {
 
     if(vars.linkColorsInTL) {
         let tlUsers = tl.map(t => t.user.screen_name).filter(u => !linkColors[u]);
-        try {
-            await fetch(`https://dimden.dev/services/twitter_link_colors/get_multiple/${tlUsers.join(',')}`)
-        } catch(e) {
-            console.error(e);
-        }
         let linkData = await fetch(`https://dimden.dev/services/twitter_link_colors/get_multiple/${tlUsers.join(',')}`).then(res => res.json()).catch(console.error);
         if(linkData) for(let i in linkData) {
             linkColors[linkData[i].username] = linkData[i].color;
