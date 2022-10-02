8f35f195e0845def9031fc1ce78f0ba0ddaf69a0
dimden <firefox78888@gmail.com>
2022-10-02 16:26:04 +0300

    fix user preview bug

--- layouts/header/script.js
+++ layouts/header/script.js
@@ -1180,11 +1180,15 @@ setTimeout(async () => {
             el = el.closest('a');
             if(!el) return;
             let url;
-            try { url = new URL(el.href) } catch(e) { return };
+            try { url = new URL(el.href.split('?')[0].split('#')[0]) } catch(e) { return };
             if(!isProfilePath(url.pathname) || url.host !== 'twitter.com') return;
             let username = url.pathname.slice(1);
+            
             if(location.pathname.slice(1) === username) return;
             if(username === user.screen_name) return;
+            if(typeof pageUser !== 'undefined') {
+                if(username === pageUser.screen_name) return;
+            }
             userPreviewTimeouts.push(setTimeout(async () => {
                 let user = await API.getUser(username, false);
                 let userPreview = document.createElement('div');
