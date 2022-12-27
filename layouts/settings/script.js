604e895db2994503a31eda7a8b6286e97504a079
dimden <firefox78888@gmail.com>
2022-12-27 15:47:12 +0200

    display all fonts in settings

--- layouts/settings/script.js
+++ layouts/settings/script.js
@@ -82,28 +82,35 @@ setTimeout(async () => {
         return;
     }
 
-    const fontCheck = new Set([
-        // Windows 10
-      'Arial', 'Arial Black', 'Bahnschrift', 'Calibri', 'Cambria', 'Cambria Math', 'Candara', 'Comic Sans MS', 'Consolas', 'Constantia', 'Corbel', 'Courier New', 'Ebrima', 'Franklin Gothic Medium', 'Gabriola', 'Gadugi', 'Georgia', 'HoloLens MDL2 Assets', 'Impact', 'Ink Free', 'Javanese Text', 'Leelawadee UI', 'Lucida Console', 'Lucida Sans Unicode', 'Malgun Gothic', 'Marlett', 'Microsoft Himalaya', 'Microsoft JhengHei', 'Microsoft New Tai Lue', 'Microsoft PhagsPa', 'Microsoft Sans Serif', 'Microsoft Tai Le', 'Microsoft YaHei', 'Microsoft Yi Baiti', 'MingLiU-ExtB', 'Mongolian Baiti', 'MS Gothic', 'MS UI Gothic', 'MV Boli', 'Myanmar Text', 'Nirmala UI', 'Palatino Linotype', 'Segoe MDL2 Assets', 'Segoe Print', 'Segoe Script', 'Segoe UI', 'Segoe UI Historic', 'Segoe UI Emoji', 'Segoe UI Symbol', 'SimSun', 'Sitka', 'Sylfaen', 'Symbol', 'Tahoma', 'Times New Roman', 'Trebuchet MS', 'Verdana', 'Webdings', 'Wingdings', 'Yu Gothic',
-        // macOS
-        'American Typewriter', 'Andale Mono', 'Arial', 'Arial Black', 'Arial Narrow', 'Arial Rounded MT Bold', 'Arial Unicode MS', 'Avenir', 'Avenir Next', 'Avenir Next Condensed', 'Baskerville', 'Big Caslon', 'Bodoni 72', 'Bodoni 72 Oldstyle', 'Bodoni 72 Smallcaps', 'Bradley Hand', 'Brush Script MT', 'Chalkboard', 'Chalkboard SE', 'Chalkduster', 'Charter', 'Cochin', 'Comic Sans MS', 'Copperplate', 'Courier', 'Courier New', 'Didot', 'DIN Alternate', 'DIN Condensed', 'Futura', 'Geneva', 'Georgia', 'Gill Sans', 'Helvetica', 'Helvetica Neue', 'Herculanum', 'Hoefler Text', 'Impact', 'Lucida Grande', 'Luminari', 'Marker Felt', 'Menlo', 'Microsoft Sans Serif', 'Monaco', 'Noteworthy', 'Optima', 'Palatino', 'Papyrus', 'Phosphate', 'Rockwell', 'Savoye LET', 'SignPainter', 'Skia', 'Snell Roundhand', 'Tahoma', 'Times', 'Times New Roman', 'Trattatello', 'Trebuchet MS', 'Verdana', 'Zapfino',
-        // other
-        'Terminus', 'Terminus (TTF)', 'Terminus (TTF) for Windows'
-    ].sort());
-      
-      let fonts = await (async() => {
-        await document.fonts.ready;
-      
-        const fontAvailable = new Set();
-      
-        for (const font of fontCheck.values()) {
-          if (document.fonts.check(`12px "${font}"`)) {
-            fontAvailable.add(font);
-          }
-        }
-      
-        return [...fontAvailable.values()];
-    })();
+    let fonts = [];
+    if(await navigator.permissions.query({name: 'local-fonts'})) {
+        fonts = [...new Set((await queryLocalFonts()).map(i => i.fullName))].filter(f => {
+            return document.fonts.check(`12px "${f}"`);
+        });
+    } else {
+        const fontCheck = new Set([
+            // Windows 10
+        'Arial', 'Arial Black', 'Bahnschrift', 'Calibri', 'Cambria', 'Cambria Math', 'Candara', 'Comic Sans MS', 'Consolas', 'Constantia', 'Corbel', 'Courier New', 'Ebrima', 'Franklin Gothic Medium', 'Gabriola', 'Gadugi', 'Georgia', 'HoloLens MDL2 Assets', 'Impact', 'Ink Free', 'Javanese Text', 'Leelawadee UI', 'Lucida Console', 'Lucida Sans Unicode', 'Malgun Gothic', 'Marlett', 'Microsoft Himalaya', 'Microsoft JhengHei', 'Microsoft New Tai Lue', 'Microsoft PhagsPa', 'Microsoft Sans Serif', 'Microsoft Tai Le', 'Microsoft YaHei', 'Microsoft Yi Baiti', 'MingLiU-ExtB', 'Mongolian Baiti', 'MS Gothic', 'MS UI Gothic', 'MV Boli', 'Myanmar Text', 'Nirmala UI', 'Palatino Linotype', 'Segoe MDL2 Assets', 'Segoe Print', 'Segoe Script', 'Segoe UI', 'Segoe UI Historic', 'Segoe UI Emoji', 'Segoe UI Symbol', 'SimSun', 'Sitka', 'Sylfaen', 'Symbol', 'Tahoma', 'Times New Roman', 'Trebuchet MS', 'Verdana', 'Webdings', 'Wingdings', 'Yu Gothic',
+            // macOS
+            'American Typewriter', 'Andale Mono', 'Arial', 'Arial Black', 'Arial Narrow', 'Arial Rounded MT Bold', 'Arial Unicode MS', 'Avenir', 'Avenir Next', 'Avenir Next Condensed', 'Baskerville', 'Big Caslon', 'Bodoni 72', 'Bodoni 72 Oldstyle', 'Bodoni 72 Smallcaps', 'Bradley Hand', 'Brush Script MT', 'Chalkboard', 'Chalkboard SE', 'Chalkduster', 'Charter', 'Cochin', 'Comic Sans MS', 'Copperplate', 'Courier', 'Courier New', 'Didot', 'DIN Alternate', 'DIN Condensed', 'Futura', 'Geneva', 'Georgia', 'Gill Sans', 'Helvetica', 'Helvetica Neue', 'Herculanum', 'Hoefler Text', 'Impact', 'Lucida Grande', 'Luminari', 'Marker Felt', 'Menlo', 'Microsoft Sans Serif', 'Monaco', 'Noteworthy', 'Optima', 'Palatino', 'Papyrus', 'Phosphate', 'Rockwell', 'Savoye LET', 'SignPainter', 'Skia', 'Snell Roundhand', 'Tahoma', 'Times', 'Times New Roman', 'Trattatello', 'Trebuchet MS', 'Verdana', 'Zapfino',
+            // other
+            'Terminus', 'Terminus (TTF)', 'Terminus (TTF) for Windows'
+        ].sort());
+        
+        fonts = await (async() => {
+            await document.fonts.ready;
+        
+            const fontAvailable = new Set();
+        
+            for (const font of fontCheck.values()) {
+                if (document.fonts.check(`12px "${font}"`)) {
+                    fontAvailable.add(font);
+                }
+            }
+        
+            return [...fontAvailable.values()];
+        })();
+    }
     let fontElement = document.getElementById('font');
     let linkColor = document.getElementById('link-color');
     let profileLinkColor = document.getElementById('profile-link-color');
@@ -133,7 +140,12 @@ setTimeout(async () => {
     let seeTweetViews = document.getElementById('see-tweet-views');
 
     let root = document.querySelector(":root");
-
+    {
+        let option = document.createElement('option');
+        option.value = "_custom";
+        option.innerText = '<CUSTOM FONT>';
+        fontElement.append(option);
+    }
     for(let i in fonts) {
         let font = fonts[i];
         let option = document.createElement('option');
@@ -144,6 +156,9 @@ setTimeout(async () => {
     }
     fontElement.addEventListener('change', () => {
         let font = fontElement.value;
+        if(font === '_custom') {
+            font = prompt('Enter a custom font name');
+        }
         root.style.setProperty('--font', `"${font}"`);
         chrome.storage.sync.set({
             font: font
