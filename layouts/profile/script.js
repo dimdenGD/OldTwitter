e3eefe10e53a8aa9db7f1a93539bf770bfab3948
0xWerz <moussarabie40@gmail.com>
2023-07-05 08:57:37 +0100

    proper en fields

--- layouts/profile/script.js
+++ layouts/profile/script.js
@@ -315,16 +315,7 @@ async function updateTimeline() {
                     tl = tl.tweets;
                 }
             } else {
-                document.getElementById(
-                    "timeline"
-                ).innerHTML = `<div dir="auto" style="padding: 50px;color: var(--darker-gray); font-size: 20px;"><h2>${
-                    LOC.user_protected.message
-                }</h2><p href="https://twitter.com/${
-                    pageUser.screen_name
-                }">${LOC.user_protected.description.replace(
-                    "%s",
-                    pageUser.screen_name
-                )}`;
+                document.getElementById("timeline").innerHTML = `<div dir="auto" style="padding: 50px;color: var(--darker-gray); font-size: 20px;"><h2>${LOC.user_protected.message}</h2><p href="https://twitter.com/${pageUser.screen_name}">${LOC.user_protected.description.replace("%s",pageUser.screen_name)}`;
                 return;
             }
         } catch(e) {
