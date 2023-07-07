9d8ff37ab808a5968d9e8bbcff0aa7a3b550dfaa
dimden <admin@dimden.dev>
2023-07-07 21:55:36 +0300

    allow resizing of reply box

--- layouts/settings/script.js
+++ layouts/settings/script.js
@@ -113,7 +113,7 @@ setTimeout(async () => {
         // macOS
         'American Typewriter', 'Andale Mono', 'Arial', 'Arial Black', 'Arial Narrow', 'Arial Rounded MT Bold', 'Arial Unicode MS', 'Avenir', 'Avenir Next', 'Avenir Next Condensed', 'Baskerville', 'Big Caslon', 'Bodoni 72', 'Bodoni 72 Oldstyle', 'Bodoni 72 Smallcaps', 'Bradley Hand', 'Brush Script MT', 'Chalkboard', 'Chalkboard SE', 'Chalkduster', 'Charter', 'Cochin', 'Comic Sans MS', 'Copperplate', 'Courier', 'Courier New', 'Didot', 'DIN Alternate', 'DIN Condensed', 'Futura', 'Geneva', 'Georgia', 'Gill Sans', 'Helvetica', 'Helvetica Neue', 'Herculanum', 'Hoefler Text', 'Impact', 'Lucida Grande', 'Luminari', 'Marker Felt', 'Menlo', 'Microsoft Sans Serif', 'Monaco', 'Noteworthy', 'Optima', 'Palatino', 'Papyrus', 'Phosphate', 'Rockwell', 'Savoye LET', 'SignPainter', 'Skia', 'Snell Roundhand', 'Tahoma', 'Times', 'Times New Roman', 'Trattatello', 'Trebuchet MS', 'Verdana', 'Zapfino',
         // other
-        'Terminus', 'Terminus (TTF)', 'Terminus (TTF) for Windows'
+        'Terminus', 'Terminus (TTF)', 'Terminus (TTF) for Windows', 'Chirp'
     ].sort());
       
       let fonts = await (async() => {
