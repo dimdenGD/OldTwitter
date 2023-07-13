cd77eb88635537210285d69e4ba96f7a6f7cad9a
Luna <87461596+LunaXCBN@users.noreply.github.com>
2023-07-13 21:12:09 +0300

    Remove lines for hiding values only

--- layouts/header/script.js
+++ layouts/header/script.js
@@ -83,9 +83,7 @@ function hideStuff() {
     }
     if(vars.hideFollowers) {
         hideStyle.innerHTML += `
-            #user-followers { display: none !important; }
             #user-followers-div { display: none !important; }
-            #profile-stat-followers-value { display: none !important; }
             #profile-stat-followers-link { display: none !important; }
         `;
     }
