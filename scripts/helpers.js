10871ddfed99c37a1e3d544d7e3c968ba17c82f5
wintiger0222 <0xodbs02@naver.com>
2023-07-28 14:29:29 +0900

    Replace to comma

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -1615,7 +1615,7 @@ async function appendTweet(t, timelineContainer, options = {}) {
                 !options.noTop &&
                 !location.pathname.includes('/status/') &&
                 !vars.useOldStyleReply ? /*html*/`
-            <div class="tweet-reply-to"><span>${LOC.replying_to_user.message.replace('$SCREEN_NAME$', mentionedUserText)}</span></div>
+            <div class="tweet-reply-to"><span>${LOC.replying_to_user.message.replace('$SCREEN_NAME$', mentionedUserText.replace(`> <`,`>, <`))}</span></div>
             `: ''}
             <div class="tweet-body ${options.mainTweet ? 'tweet-body-main' : ''}">
                 <span class="tweet-body-text ${vars.noBigFont || t.full_text.length > 280 || !options.bigFont || (!options.mainTweet && location.pathname.includes('/status/')) ? 'tweet-body-text-long' : 'tweet-body-text-short'}">${t.in_reply_to_screen_name && vars.useOldStyleReply ? /*html*/mentionedUserText: ''}${full_text ? await renderTweetBodyHTML(full_text, t.entities, t.display_text_range) : ''}</span>
