3188305e86343584b86aa3dc4b9cfb0e1fecaa0a
dimden <admin@dimden.dev>
2023-08-04 13:39:30 +0300

    reduce useless requests

--- scripts/apis.js
+++ scripts/apis.js
@@ -944,9 +944,16 @@ const API = {
         get: (cursor, onlyMentions = false, cache = true) => {
             return new Promise((resolve, reject) => {
                 chrome.storage.local.get(['notifications'], d => {
-                    if(d.notifications && Date.now() - d.notifications.date < 60000 && !cursor && !onlyMentions && cache) {
-                        debugLog('notifications.get', 'cache', d.notifications.data);
-                        return resolve(d.notifications.data);
+                    if(cache) {
+                        if(d.notifications && Date.now() - d.notifications.date < 60000 && !cursor && !onlyMentions) {
+                            debugLog('notifications.get', 'cache', d.notifications.data);
+                            return resolve(d.notifications.data);
+                        }
+                    } else {
+                        if(d.notifications && Date.now() - d.notifications.date < 15000 && !cursor && !onlyMentions) {
+                            debugLog('notifications.get', 'cache', d.notifications.data);
+                            return resolve(d.notifications.data);
+                        }
                     }
                     fetch(`https://twitter.com/i/api/2/notifications/${onlyMentions ? 'mentions' : 'all'}.json?include_profile_interstitial_type=1&include_blocking=1&include_blocked_by=1&include_followed_by=1&include_want_retweets=1&include_mute_edge=1&include_can_dm=1&include_can_media_tag=1&include_ext_has_nft_avatar=1&include_ext_is_blue_verified=1&include_ext_verified_type=1&include_ext_profile_image_shape=1&skip_status=1&cards_platform=Web-12&include_cards=1&include_ext_alt_text=true&include_ext_limited_action_results=true&include_quote_count=true&include_reply_count=1&tweet_mode=extended&include_ext_views=true&include_entities=true&include_user_entities=true&include_ext_media_color=true&include_ext_media_availability=true&include_ext_sensitive_media_warning=true&include_ext_trusted_friends_metadata=true&send_error_codes=true&simple_quoted_tweet=true&count=20&requestContext=launch&ext=mediaStats%2ChighlightedLabel%2ChasNftAvatar%2CvoiceInfo%2CbirdwatchPivot%2CsuperFollowMetadata%2CunmentionInfo%2CeditControl${cursor ? `&cursor=${cursor}` : ''}`, {
                         headers: {
