2beaf64d432e173a640001c3f3d41efb89ac2108
shy <70299052+whakama@users.noreply.github.com>
2023-11-25 10:23:09 -0600

    to be clear

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -3737,7 +3737,7 @@ function renderNotification(n, options = {}) {
 
                         var userJoinDate = new Date(user.created_at).getTime();
                         var timeSinceJoin = Date.now() - userJoinDate;
-                        var yearsSinceJoin = Math.floor(timeSinceJoin / 31556952000);
+                        var yearsSinceJoin = Math.floor(timeSinceJoin / 31556952000); //ms is 1 year
                         var anniversaryPicUrl = `https://ton.twimg.com/ntab_public/twitterversary/year${yearsSinceJoin}.jpg`;
                         var anniversaryPicBlob = await (await fetch(anniversaryPicUrl)).blob();
                         var anniversaryPicFile = new File([anniversaryPicBlob], `year${yearsSinceJoin}.jpg`, { type: 'image/jpeg' });
