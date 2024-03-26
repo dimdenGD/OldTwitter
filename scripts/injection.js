34a8f48f3aa08848537757060f64838166be20f3
dimden <26517362+dimdenGD@users.noreply.github.com>
2024-03-26 23:38:09 +0200

    device id

--- scripts/injection.js
+++ scripts/injection.js
@@ -236,6 +236,50 @@ function isDark() {
 }
 let customCSS, profileCSS = false;
 
+function uuidV4() {
+    const uuid = new Array(36);
+    for (let i = 0; i < 36; i++) {
+      uuid[i] = Math.floor(Math.random() * 16);
+    }
+    uuid[14] = 4; // set bits 12-15 of time-high-and-version to 0100
+    uuid[19] = uuid[19] &= ~(1 << 2); // set bit 6 of clock-seq-and-reserved to zero
+    uuid[19] = uuid[19] |= (1 << 3); // set bit 7 of clock-seq-and-reserved to one
+    uuid[8] = uuid[13] = uuid[18] = uuid[23] = '-';
+    return uuid.map((x) => x.toString(16)).join('');
+}
+
+async function readCryptoKey() {
+    return new Promise((resolve, reject) => {
+        let request = indexedDB.open("localforage");
+
+        request.onerror = function(event) {
+            reject(event);
+        };
+
+        request.onsuccess = function(event) {
+            const db = event.target.result;
+
+            // Open a transaction to access the keyvaluepairs object store
+            const transaction = db.transaction(['keyvaluepairs'], 'readonly');
+            const objectStore = transaction.objectStore('keyvaluepairs');
+          
+            objectStore.openCursor().onsuccess = function(event) {
+                const cursor = event.target.result;
+                if (cursor) {
+                    // Check if the key matches the pattern
+                    const key = cursor.key;
+                    if (key.startsWith('device:rweb.dmCryptoKeys')) {
+                        resolve(cursor.value);
+                    }
+            
+                    // Move to the next entry
+                    cursor.continue();
+                }
+            };
+        };
+    });
+}
+
 async function openDatabase() {
     return new Promise((resolve, reject) => {
         let request = indexedDB.open("CustomCSSDatabase", 1);
@@ -567,6 +611,19 @@ let page = realPath === "" ? pages[0] : pages.find(p => (!p.exclude || !p.exclud
         for (let i = 0; i < msgs.length; i++) {
             let m = msgs[i].slice(6, -2);
             if(LOC[m]) header_html = replaceAll(header_html, msgs[i], LOC[m].message);
+        }
+    }
+
+    OLDTWITTER_CONFIG.verificationKey = document.querySelector('meta[name="twitter-site-verification"]')?.content;
+
+    let cryptoKey = await readCryptoKey();
+    if(cryptoKey) {
+        OLDTWITTER_CONFIG.deviceId = cryptoKey.deviceId;
+    } else {
+        OLDTWITTER_CONFIG.deviceId = localStorage.getItem('device_id');
+        if(!OLDTWITTER_CONFIG.deviceId) {
+            OLDTWITTER_CONFIG.deviceId = uuidV4();
+            localStorage.setItem('device_id', OLDTWITTER_CONFIG.deviceId);
         }
     }
 
