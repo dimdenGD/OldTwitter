9bfaf08e9dd90fee81ef99bff20a71d28a7ece32
dimden <26517362+dimdenGD@users.noreply.github.com>
2024-04-01 01:17:07 +0300

    update changelog

--- layouts/home/script.js
+++ layouts/home/script.js
@@ -81,9 +81,7 @@ setTimeout(() => {
                     <h2 style="margin:0;margin-bottom:10px;color:var(--darker-gray);font-weight:300">(OldTwitter) ${LOC.new_version.message} - ${chrome.runtime.getManifest().version}</h2>
                     <span id="changelog" style="font-size:14px;color:var(--default-text-color)">
                         <ul>
-                            <li>Added new languages and updated translations.</li>
-                            <li>Small bug fixes.</li>
-                            <li>Check out <a href="https://whispy.org/" target="_blank">whispy.org</a> if you're looking for actually good Twitter alternative (from creator of this extension)</li>
+                            <li>Maybe fixed logouts, still unsure</li>
                         </ul>
                     </span>
                 `, 'changelog-modal', () => {}, () => Date.now() - opened > 1250);
