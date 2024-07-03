a6a2e08dffed6a1d122103ef50d5ae48ca05da3e
shy <70299052+shy1132@users.noreply.github.com>
2024-07-03 04:34:08 -0500

    refine firefox fix thing

--- scripts/background_v2.js
+++ scripts/background_v2.js
@@ -59,17 +59,21 @@ chrome.webRequest.onBeforeSendHeaders.addListener(
     },
     ["blocking", "requestHeaders"]
 );
-chrome.webRequest.onBeforeSendHeaders.addListener(
+chrome.webRequest.onBeforeSendHeaders.addListener( //this isnt particularly elegant solution but i dont want to risk doing anything that may trigger bot protection or something like that
     function(details) {
         for (let i = 0; i < details.requestHeaders.length; i++) {
             if (details.requestHeaders[i].name.toLowerCase() === 'user-agent') {
                 if (details.requestHeaders[i].value.toLowerCase().includes('firefox')) {
-                    let rvRegex = /rv:(\d+\.\d+)/;
-                    let rvMatch = details.requestHeaders[i].value.match(rvRegex);
-                    if (rvMatch) {
-                        let rv = parseFloat(rvMatch[1]);
-                        if (rv < 110) {
-                            details.requestHeaders[i].value = details.requestHeaders[i].value.replace(rvRegex, 'rv:110.0'); //twitter serves client-web-legacy if rv is less than 110, which breaks request signing
+                    let rvRegex = /rv:(\d+\.\d+)/; //gecko version (whats important here)
+                    let versionRegex = /Firefox\/(\d+(?:\.\d+)+)/; //browser version
+                    let versionMatch = details.requestHeaders[i].value.match(versionRegex);
+                    if (versionMatch) {
+                        let version = parseFloat(versionMatch[1]);
+                        let fallback = '127.0'; //if this ever breaks maybe set this to latest firefox version
+                        if (version < 110) { //rv 110 is cutoff point between client-web-legacy and client-web, so we should just spoof browser and rv to latest
+                            details.requestHeaders[i].value = details.requestHeaders[i].value.replace(rvRegex, `rv:${fallback}`).replace(versionRegex, `Firefox/${fallback}`);
+                        } else {
+                            details.requestHeaders[i].value = details.requestHeaders[i].value.replace(rvRegex, `rv:${version}.0`);
                         }
                     }
                 }
