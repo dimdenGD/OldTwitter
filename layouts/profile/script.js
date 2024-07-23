d8cc5d11130466cb584afbc4882ac8d4ab9dee28
shy <70299052+shy1132@users.noreply.github.com>
2024-07-22 21:12:48 -0500

    fix profile url on abandoned profiles

--- layouts/profile/script.js
+++ layouts/profile/script.js
@@ -1775,8 +1775,8 @@ async function renderProfile() {
         let url = document.createElement('a');
         url.classList.add('profile-additional-thing', 'profile-additional-url');
         let realUrl = pageUser.entities.url.urls[0];
-        url.innerText = realUrl.display_url;
-        url.href = realUrl.expanded_url;
+        url.innerText = realUrl.display_url || realUrl.url;
+        url.href = realUrl.expanded_url || realUrl.url;
         if(!url.href.startsWith('/')) url.target = "_blank";
         additionalInfo.appendChild(url);
     }
