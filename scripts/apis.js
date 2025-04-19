807e12b9e20a3781fc1bc59f12ebb85cfcbaa804
dimden <admin@dimden.dev>
2025-04-19 14:32:15 +0300

    fix

--- scripts/apis.js
+++ scripts/apis.js
@@ -1772,14 +1772,16 @@ const API = {
                         if (data.errors && data.errors[0].code === 32) {
                             return reject("Not logged in");
                         }
-                        if (data.errors && data.errors[0] && !data.data?.user?.result?.timeline_v2?.timeline?.instructions) {
+                        let ix = data.data?.user?.result?.timeline_v2?.timeline?.instructions;
+                        if(!ix) ix = data.data?.user?.result?.timeline?.timeline?.instructions;
+                        if (data.errors && data.errors[0] && !ix) {
                             if(data.errors[0].code === 88 && typeof useDiffKey === 'undefined') {
                                 localStorage.hitRateLimit = Date.now() + 1000 * 60 * 10;
                                 return API.user.getTweetsV2(id, cursor, replies, true).then(resolve).catch(reject);
                             }
                             return reject(data.errors[0].message);
                         }
-                        let instructions = data.data.user.result.timeline_v2.timeline.instructions;
+                        let instructions = ix;
                         let entries = instructions.find(e => e.type === "TimelineAddEntries");
                         if(!entries) {
                             return reject("Nothing here");
