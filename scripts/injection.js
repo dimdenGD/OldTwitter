feec147936a9f6e955ff23e5a13036a0726e84fe
catboxanon <122327233+catboxanon@users.noreply.github.com>
2025-11-17 11:20:28 -0500

    Fix loading XChat

--- scripts/injection.js
+++ scripts/injection.js
@@ -147,6 +147,7 @@ if (
         realPath.startsWith("/i/premium_sign_up/") ||
         realPath.startsWith("/i/events/") ||
         realPath.startsWith("/i/spaces/") ||
+        realPath.startsWith("/i/chat") ||
         realPath.startsWith("/i/oauth2") ||
         realPath.startsWith("/account") ||
         realPath.startsWith("/settings") ||
